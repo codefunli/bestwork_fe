aef030ee6335ca38d6cbe6624c1545a9cf39964a
pecom123 <pecom123@gmail.com>
2022-09-26 16:24:19 +0700

    [CRM][FE] update login screen

--- src/ui/login/login.tsx
+++ src/ui/login/login.tsx
@@ -1,12 +1,36 @@
-import { AccountCircle, PasswordOutlined } from "@mui/icons-material";
+import { AccountCircle, PasswordOutlined, Visibility, VisibilityOff } from "@mui/icons-material";
 import { Button, FormControl, IconButton, InputAdornment, InputLabel, OutlinedInput, Typography } from "@mui/material";
 import { red } from "@mui/material/colors";
+import { useState } from "react";
 import "./login.scss";
 
+const initialValues = {
+    user: "",
+    password: "",
+};
+
 export default function Login() {
+    const [formValues, setFormValues] = useState(initialValues);
+    const [showPassword, setShowPassword] = useState(false);
+    const handleChangeValue = (event: any) => {
+        const { name, value } = event.target;
+        setFormValues({
+          ...formValues,
+          [name]: value,
+        });
+    }
+    
+    const handleClickShowPassword = () => {
+        setShowPassword(!showPassword);
+    }
+
+    const handleLogin = () => {
+        alert("Login in here")
+    }
     return (
         <div className="login-wrapper">
             <div className="login-form-wrapper">
+            <form>
                 <div className="login-form">
                     <Typography
                         variant='h6'
@@ -15,58 +39,57 @@ export default function Login() {
                     >
                         LOG INTO YOUR ACCOUNT
                     </Typography>
-                <div className="login-form-field">
-                    <AccountCircle sx={{ mr: 1, my: 0.5}} />
-                    <FormControl sx={{ m: 1, width: '30ch', color:red }} variant="outlined">
-                        <InputLabel required htmlFor="outlined-adornment-password" sx={{color:"white"}}>Account</InputLabel>
-                        <OutlinedInput
-                            id="outlined-adornment-password"
-                            //type={values.showPassword ? 'text' : 'password'}
-                            //value={values.password}
-                            //onChange={handleChange('password')}
-                            endAdornment={
-                            <InputAdornment position="end">
-                                <IconButton
-                                aria-label="toggle password visibility"
-                                //onClick={handleClickShowPassword}
-                                //onMouseDown={handleMouseDownPassword}
-                                edge="end"
-                                >
-                                </IconButton>
-                            </InputAdornment>
-                            }
-                            label="account"
-                        />
-                    </FormControl>
-                </div>
-                <div className="login-form-field">
-                    <PasswordOutlined sx={{ mr: 1, my: 0.5 }} />
-                    <FormControl sx={{ m: 1, width: '30ch' }} variant="outlined">
-                        <InputLabel  required htmlFor="outlined-adornment-password" sx={{color:"white"}}>
-                            Password
-                        </InputLabel>
-                        <OutlinedInput
-                            id="outlined-adornment-password"
-                            //type={values.showPassword ? 'text' : 'password'}
-                            //value={values.password}
-                            //onChange={handleChange('password')}
-                            endAdornment={
-                            <InputAdornment position="end">
-                                <IconButton
-                                aria-label="toggle password visibility"
-                                //onClick={handleClickShowPassword}
-                                //onMouseDown={handleMouseDownPassword}
-                                edge="end"
-                                >
-                                </IconButton>
-                            </InputAdornment>
-                            }
-                            label="Password"
-                        />
-                    </FormControl>
-                </div>
-                <Button variant="contained" className="justify-center">Login</Button>
-                </div>
+                    <div className="login-form-field">
+                        <AccountCircle sx={{ mr: 1, my: 0.5}} />
+                        <FormControl sx={{ m: 1, width: '30ch', color:red }} variant="outlined">
+                            <InputLabel required htmlFor="outlined-adornment-password" sx={{color:"white"}}>Account</InputLabel>
+                            <OutlinedInput
+                                id="outlined-adornment-password"
+                                type='text'
+                                name="user"
+                                value={formValues.user}
+                                onChange={handleChangeValue}
+                                endAdornment={
+                                <InputAdornment position="end">
+                                    <IconButton aria-label="toggle password visibility"
+                                    edge="end">
+                                    </IconButton>
+                                </InputAdornment>
+                                }
+                                label="account"
+                            />
+                        </FormControl>
+                    </div>
+                    <div className="login-form-field">
+                        <PasswordOutlined sx={{ mr: 1, my: 0.5 }} />
+                        <FormControl sx={{ m: 1, width: '30ch' }} variant="outlined">
+                            <InputLabel  required htmlFor="outlined-adornment-password" sx={{color:"white"}}>
+                                Password
+                            </InputLabel>
+                            <OutlinedInput
+                                id="outlined-adornment-password"
+                                type={showPassword ? 'text' : 'password'}
+                                name="password"
+                                value={formValues.password}
+                                onChange={handleChangeValue}
+                                endAdornment={
+                                <InputAdornment position="end">
+                                    <IconButton
+                                    aria-label="toggle password visibility"
+                                    onClick={handleClickShowPassword}
+                                    edge="end"
+                                    >
+                                        {showPassword ? <VisibilityOff sx={{color:"white"}}/> : <Visibility sx={{color:"white"}}/>}
+                                    </IconButton>
+                                </InputAdornment>
+                                }
+                                label="Password"
+                            />
+                        </FormControl>
+                    </div>
+                        <Button variant="outlined" sx={{color:"white"}} className="justify-center" onClick={handleLogin}>Login</Button>
+                    </div>
+            </form>
             </div>
         </div>
         
