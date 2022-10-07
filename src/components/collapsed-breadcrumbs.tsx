bbc37f00ca7297055fec957cc93e6a93331db803
nineplusv01 <nineplusv01@gmail.com>
2022-10-07 09:10:27 +0700

    [CRM][SONLV] add bread-crumbs

--- src/components/collapsed-breadcrumbs.tsx
+++ src/components/collapsed-breadcrumbs.tsx
@@ -1,32 +1,39 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
-import * as React from 'react';
-import Breadcrumbs from '@mui/material/Breadcrumbs';
-import Typography from '@mui/material/Typography';
-import Link from '@mui/material/Link';
+import {Breadcrumbs, Typography} from '@mui/material';
+import Stack from '@mui/material/Stack';
+import { useLocation, Link} from 'react-router-dom';
+import NavigateNextIcon from '@mui/icons-material/NavigateNext';
+import { UrlFeApp } from '../core/constants/common';
+import { useTranslation } from 'react-i18next';
 
-function handleClick(event: React.MouseEvent<HTMLDivElement, MouseEvent>) {
-    event.preventDefault();
-    console.info('You clicked a breadcrumb.');
-}
-
-export default function CollapsedBreadcrumbs() {
+const CollapsedBreadcrumbs = () => {
+    const location = useLocation();
+    const pathNames =location.pathname.split("/").filter(x => x).splice(1);
+    const { t } = useTranslation();
+    
     return (
-        <div role="presentation" className='d-none d-lg-block' onClick={handleClick}>
-            <Breadcrumbs maxItems={2} aria-label="breadcrumb">
-                <Link underline="hover" color="inherit" href="#">
-                    Home
-                </Link>
-                <Link underline="hover" color="inherit" href="#">
-                    Catalog
-                </Link>
-                <Link underline="hover" color="inherit" href="#">
-                    Accessories
-                </Link>
-                <Link underline="hover" color="inherit" href="#">
-                    New Collection
-                </Link>
-                <Typography color="text.primary">Belts</Typography>
-            </Breadcrumbs>
+        <div role="presentation" className='d-none d-lg-block'>
+            <Stack spacing={2}>
+                <Breadcrumbs aria-label="breadcrumb" separator={<NavigateNextIcon fontSize="small" />}>
+                    {pathNames.length > 0 ? (
+                        <Link to={UrlFeApp.DASH_BOARD} >{t('bread_crumbs.home')}</Link>
+                    ) : (
+                        <Typography >{t('bread_crumbs.home')}</Typography>
+                    )}
+                    {pathNames.map((name, index) => {
+                        const routeTo = `${UrlFeApp.MAIN_APP}/${pathNames.slice(0, index + 1).join("/")}`;
+                        console.log(routeTo);
+                        
+                        const isLast = index === pathNames.length -1;
+                        return isLast ? (
+                            <Typography color='white' key={name}> {t('bread_crumbs.' + `${name}` + '')} </Typography>
+                        ) : (
+                            
+                            <Link key={name} to={routeTo}>{t('bread_crumbs.' + `${name}` + '')}</Link>
+                        )
+                    })}
+                </Breadcrumbs>
+            </Stack>
         </div>
     );
 }
+export default CollapsedBreadcrumbs;
