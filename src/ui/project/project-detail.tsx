370b067971a9d8ece30fe14086465b91c348bbd6
nineplusv01 <114661639+nineplusv01@users.noreply.github.com>
2022-10-27 15:50:38 +0700

    [CRM][PHUONGTD][PROGRESS][LIST] connect api

--- src/ui/project/project-detail.tsx
+++ src/ui/project/project-detail.tsx
@@ -8,6 +8,7 @@ import TimelineSeparator from '@mui/lab/TimelineSeparator';
 import TimelineOppositeContent, { timelineOppositeContentClasses } from '@mui/lab/TimelineOppositeContent';
 import {
     Avatar,
+    Button,
     Card,
     CardActionArea,
     CardContent,
@@ -23,49 +24,26 @@ import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useParams } from 'react-router-dom';
 import { ProjectProgressDTO } from '../../models/project-res-dto';
-import { getProject } from '../../services/project-service';
+import { getProgressByProjectId, getProject } from '../../services/project-service';
 import ProgressCreate from './progress-create';
 import ProgressEdit from './progress-edit';
 import './project.scss';
 
-const initialValues = {
-    project: {
-        projectName: '',
-        projectType: '',
-        description: '',
-        comment: '',
-        updateDate: '',
-        notificationFlag: true,
-        isPaid: false,
-        status: '',
-    },
-};
-
 export default function ProjectEdit() {
-    const [projectData, setProjectData] = useState(initialValues);
+    const [projectData, setProjectData] = useState<any>({});
     const { t } = useTranslation();
     const params = useParams();
     const [isOpenCreateProgress, setOpenCreateProgress] = useState(false);
     const [isOpenEditProgress, setOpenEditProgress] = useState(false);
     const [selectedProgress, setSelectedProgress] = useState<any>();
-
     const [progressList, setProgressList] = useState<ProjectProgressDTO[]>([]);
 
-    const progressListFromLocal: any = localStorage.getItem('progressList');
-
     useEffect(() => {
-        const progressTmp: any = JSON.parse(progressListFromLocal);
-        if (progressTmp) setProgressList(progressTmp);
-    }, [progressListFromLocal]);
-
-    useEffect(() => {
-        if (params.id !== undefined) {
-            getProject(params.id).then((value: any) => {
-                if (value !== undefined && value.data !== undefined) {
-                    const project = {
-                        ...value.data,
-                    };
-                    setProjectData({ project });
+        if (params.id) {
+            getProgressByProjectId(params.id).then((value: any) => {
+                if (value && value.data) {
+                    setProjectData(value.data.project);
+                    setProgressList(value.data.progress);
                 }
             });
         }
@@ -99,6 +77,14 @@ export default function ProjectEdit() {
         setSelectedProgress(id);
     };
 
+    const handleCloseProject = (project: any) => {
+        console.log(project);
+    };
+
+    const renderStatusChip = (data: string) => {
+        return <Chip label={data} color="secondary" />;
+    };
+
     return (
         <div className="project-detail">
             <form>
@@ -108,18 +94,40 @@ export default function ProjectEdit() {
                             <CardActionArea>
                                 <CardMedia
                                     component="img"
-                                    height="140"
+                                    height="auto"
                                     image="https://cdn.pixabay.com/photo/2016/03/29/08/48/project-1287781__340.jpg"
                                     alt="green iguana"
                                 />
-                                <CardContent>
-                                    <Typography gutterBottom variant="h5" component="div">
-                                        Đặt cược F88
-                                    </Typography>
-                                    <Typography variant="body2" color="text.secondary">
-                                        Nhà cái tới từ Châu âu, thay đổi, lắp đặt kích thước, tranh ảnh, điện trần nhà
-                                    </Typography>
-                                </CardContent>
+                                {projectData && (
+                                    <>
+                                        <CardHeader
+                                            action={
+                                                <Button
+                                                    variant="contained"
+                                                    onClick={() => handleCloseProject(projectData)}
+                                                >
+                                                    Close
+                                                </Button>
+                                            }
+                                            className="pb-0"
+                                        ></CardHeader>
+                                        <CardContent>
+                                            <Typography gutterBottom variant="h3" component="div">
+                                                {projectData.projectName}
+                                            </Typography>
+                                            <Typography color="text.secondary" style={{ fontSize: '20px' }}>
+                                                {t('project.search.description')}: {projectData.description}
+                                            </Typography>
+                                            <Typography color="text.secondary" style={{ fontSize: '20px' }}>
+                                                {t('message.status')}: {projectData.status}
+                                            </Typography>
+                                            <Typography color="text.secondary" style={{ fontSize: '20px' }}>
+                                                {t('radio.paid')}:
+                                                {projectData.isPaid === 1 ? t('radio.paid') : t('radio.unPaid')}
+                                            </Typography>
+                                        </CardContent>
+                                    </>
+                                )}
                             </CardActionArea>
                         </Card>
                     </Grid>
@@ -144,10 +152,10 @@ export default function ProjectEdit() {
                                 }}
                             >
                                 {progressList.map((progress: ProjectProgressDTO, index: number) => (
-                                    <div>
+                                    <div key={index}>
                                         <TimelineItem>
                                             <TimelineOppositeContent color="textSecondary">
-                                                {progress.startDate}
+                                                {progress.startDate.replace(/[TZ]/g, ' ')}
                                             </TimelineOppositeContent>
                                             <TimelineSeparator className="h-40">
                                                 <TimelineDot />
@@ -155,12 +163,16 @@ export default function ProjectEdit() {
                                             </TimelineSeparator>
                                             <TimelineContent>
                                                 <div
-                                                    className="mb-4 progress-item"
+                                                    className="mb-4 progress-item pb-2"
                                                     onClick={() => handleEditProgress(progress.id)}
                                                 >
-                                                    <div className="progress-item-title">{progress.title}</div>
-                                                    <div className="progress-item-report">{progress.report}</div>
-                                                    <Chip label="pedding" color="secondary" />
+                                                    <div className="progress-item-title pb-2 h4 fw-bold">
+                                                        {progress.title}
+                                                    </div>
+                                                    <div className="progress-item-report pb-2">{progress.report}</div>
+                                                    <div className="progress-item-report pb-2">
+                                                        {renderStatusChip(progress.status)}
+                                                    </div>
                                                 </div>
                                             </TimelineContent>
                                         </TimelineItem>
