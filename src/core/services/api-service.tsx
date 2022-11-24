4b1d13c9866ba90720b1daed5e180c3a62eb7ba8
nineplusv01 <114661639+nineplusv01@users.noreply.github.com>
2022-11-24 17:06:13 +0700

    [CRM][PHUONGTD][LOGIN] fix login

--- src/core/services/api-service.tsx
+++ src/core/services/api-service.tsx
@@ -2,10 +2,10 @@ import axios from 'axios';
 import { stringify } from 'qs';
 import { BASE_API_PATH } from '../constants/urls';
 
-const accessToken = localStorage.getItem('access_token');
-const refreshToken = localStorage.getItem('refresh_token');
+let accessToken = localStorage.getItem('access_token');
+let refreshToken = localStorage.getItem('refresh_token');
 
-const apiClient = axios.create({
+let apiClient = axios.create({
     baseURL: BASE_API_PATH,
     withCredentials: true,
     headers: {
@@ -21,10 +21,23 @@ const apiClient = axios.create({
 
 apiClient.interceptors.response.use(
     (res) => {
-        const access_token = res.headers.access_token;
-        const refresh_token = res.headers.refresh_token;
-        localStorage.setItem('access_token', access_token);
-        localStorage.setItem('refresh_token', refresh_token);
+        const accessTokenRes = res.headers.access_token;
+        const refreshTokenRes = res.headers.refresh_token;
+        localStorage.setItem('access_token', accessTokenRes);
+        localStorage.setItem('refresh_token', refreshTokenRes);
+        apiClient = axios.create({
+            baseURL: BASE_API_PATH,
+            withCredentials: true,
+            headers: {
+                'content-type': 'application/json',
+                prefix: 'Bearer',
+                access_token: accessTokenRes,
+                refresh_token: refreshTokenRes,
+            },
+            paramsSerializer(params: any) {
+                return stringify(params);
+            },
+        });
         return res;
     },
     (err) => {
